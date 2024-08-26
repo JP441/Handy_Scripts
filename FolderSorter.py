b0b55ff9cd0b5d86df226d1989ca8bad931c72dc
s3935138 <s3935138@student.rmit.edu.au>
2024-08-26 22:24:25 +1000

    feat : add folders can be reused

--- FolderSorter.py
+++ FolderSorter.py
@@ -6,26 +6,28 @@ def make_sorted_folder(folder_name="sorted_folder"):
 	try:
 		os.mkdir(folder_name)
 		return folder_name
-	except os.FileExistError:
-		print(f"directory named {folder_name} already exists")
-		sys.exit()
-	except os.FileNotFoundError:
+	except FileExistsError:
+		return folder_name
+	except FileNotFoundError:
 		print(f"") #TO add
 		sys.exit()
 
 def make_sub_dirs(files):
-	file_types = set()
-	no_dir_files = [] #files that are not directories
-	for x in files:
-		if os.path.isfile(x):
-			file_types.add(os.path.splitext(x)[1].strip(".").strip())
-			no_dir_files.append(x)
-	for x in file_types:
-		if x != '':
-			os.mkdir(x)
-		else:
-			os.mkdir("no_file_type")
-	return no_dir_files, file_types
+		file_types = set()
+		no_dir_files = [] #files that are not directories
+		for x in files:
+			if os.path.isfile(x):
+				file_types.add(os.path.splitext(x)[1].strip(".").strip())
+				no_dir_files.append(x)
+		for x in file_types:
+			try:
+				if x != '':
+					os.mkdir(x)
+				else:
+					os.mkdir("no_file_type")
+			except FileExistsError:
+				pass
+		return no_dir_files, file_types
 
 def move_all_files_to_sorted(folder_name):
 	files = os.listdir()
@@ -43,11 +45,6 @@ def move_all_files_to_sub_dir(files, file_types):
 		else:
 			current_file_type = file.rsplit(".")[-1].strip()
 		shutil.move(file, current_file_type)
-
-
-
-
-
 
 folder_name = make_sorted_folder()
 files = move_all_files_to_sorted(folder_name)

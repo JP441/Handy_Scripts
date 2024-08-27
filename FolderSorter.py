ec408f1a9721cac2aebeb4d489c3e2ce328f4127
s3935138 <s3935138@student.rmit.edu.au>
2024-08-27 19:50:59 +1000

    refactor : converted code toa class

--- FolderSorter.py
+++ FolderSorter.py
@@ -2,53 +2,74 @@ import os
 import sys
 import shutil
 
-def make_sorted_folder(folder_name="sorted_folder"):
-	try:
-		os.mkdir(folder_name)
-		return folder_name
-	except FileExistsError:
-		return folder_name
-	except FileNotFoundError:
-		print(f"") #TO add
-		sys.exit()
+class FolderSorter():
+	def __init__(self):
+		self.folder_name = "sorted_folder"
+		self.move_folder = True
+		self.all_files = os.listdir()
+		print(self.all_files)
+		self.no_dir_files = []
+		self.file_types = set()
 
-def make_sub_dirs(files):
-		file_types = set()
-		no_dir_files = [] #files that are not directories
-		for x in files:
-			if os.path.isfile(x):
-				file_types.add(os.path.splitext(x)[1].strip(".").strip())
-				no_dir_files.append(x)
-		for x in file_types:
+	def make_sorted_folder(self):
+		try:
+			os.mkdir(self.folder_name)
+		except FileExistsError:
+			pass
+		except FileNotFoundError:
+			print(f"") #TO add
+			sys.exit()
+
+	def make_sub_dirs(self):
+			for x in self.no_dir_files:
+				self.file_types.add(os.path.splitext(x)[1].strip(".").strip())
+			for x in self.file_types:
+				try:
+					if x != '':
+						os.mkdir(x)
+					else:
+						os.mkdir("no_file_type")
+				except FileExistsError:
+					pass
+
+	def move_all_files_to_sorted(self):
+		if not self.move_folder:
+			for file in self.no_dir_files:
+				try:
+					shutil.move(file, self.folder_name)
+				except shutil.Error: 
+					print(f"Could not move {file} to {self.folder_name} "
+					 "as it already exists")
+			return
+		for file in self.all_files:
 			try:
-				if x != '':
-					os.mkdir(x)
-				else:
-					os.mkdir("no_file_type")
-			except FileExistsError:
-				pass
-		return no_dir_files, file_types
+				shutil.move(file, self.folder_name)
+			except shutil.Error: 
+				print(f"Could not move {file} to {self.folder_name} "
+				 "as it already exists")
 
-def move_all_files_to_sorted(folder_name):
-	files = os.listdir()
-	for file in files:
-		if os.path.isfile(file):
-			shutil.move(file, folder_name)
-	return files
+	def get_files_that_are_not_dir(self):
+		self.no_dir_files = [x for x in self.all_files if os.path.isfile(x)]
+
+	def move_all_files_to_sub_dir(self):
+		for file in self.no_dir_files:
+			if len(file.rsplit(".")) == 1:
+				current_file_type = "no_file_type"
+			else:
+				current_file_type = file.rsplit(".")[-1].strip()
+			shutil.move(file, current_file_type)
+
+	def run_program(self):
+		self.make_sorted_folder()
+		self.get_files_that_are_not_dir()
+		self.move_all_files_to_sorted()
+		os.chdir(f"{self.folder_name}")
+		self.make_sub_dirs()
+		self.move_all_files_to_sub_dir()
+
+fs = FolderSorter()
+fs.run_program()
+	
 
 
-def move_all_files_to_sub_dir(files, file_types):
-	print(files)
-	for file in files:
-		if len(file.rsplit(".")) == 1:
-			current_file_type = "no_file_type"
-		else:
-			current_file_type = file.rsplit(".")[-1].strip()
-		shutil.move(file, current_file_type)
-
-folder_name = make_sorted_folder()
-files = move_all_files_to_sorted(folder_name)
-os.chdir(f"{folder_name}")
-files, file_types = make_sub_dirs(files)
-move_all_files_to_sub_dir(files, file_types)
 
